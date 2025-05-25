6ad053ee4f285ee181a71a35394c103077251f91
Daniel R <drestrepo@foqum.io>
2025-05-25 15:33:01 +0200

    :sparkles: Added the endpoint feedback in /register folder

--- register/main.py
+++ register/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime  # ← NUEVO
+from datetime import datetime
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
@@ -29,6 +29,11 @@ class Usuario(BaseModel):
     gmail: str
 
 
+class Sugerencia(BaseModel):
+    gmail: str
+    sugerencia: str
+
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive",
@@ -36,18 +41,27 @@ SCOPE = [
 SPREADSHEET_NAME = "registro-usuarios"
 
 
-def get_sheet():
+def get_sheet(sheet_name="Hoja 1"):
     creds_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
     creds_dict = json.loads(creds_json)
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
     client = gspread.authorize(creds)
-    sheet = client.open(SPREADSHEET_NAME).sheet1
+    spreadsheet = client.open(SPREADSHEET_NAME)
+    sheet = spreadsheet.worksheet(sheet_name)
     return sheet
 
 
 @app.post("/registrar")
 def registrar_usuario(usuario: Usuario):
-    sheet = get_sheet()
-    fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # ← FECHA ACTUAL
-    sheet.append_row([usuario.nombre, usuario.gmail, fecha_actual])  # ← AÑADE FECHA
+    sheet = get_sheet("registros")
+    fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sheet.append_row([usuario.nombre, usuario.gmail, fecha_actual])
     return {"mensaje": "Usuario registrado correctamente"}
+
+
+@app.post("/feedback")
+def guardar_sugerencia(data: Sugerencia):
+    sheet = get_sheet("sugerencias")
+    fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sheet.append_row([data.gmail, data.sugerencia, fecha_actual])
+    return {"mensaje": "Sugerencia guardada correctamente"}
